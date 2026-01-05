f0a662febf4576b51b56f80b92141127c545a6d1
Rucha Mahabal <ruchamahabal2@gmail.com>
2026-01-05 18:55:54 +0530

    fix: data attributes not falling through compiled template

--- frontend/src/components/AppLayout/HTML.vue
+++ frontend/src/components/AppLayout/HTML.vue
@@ -1,13 +1,14 @@
 <template>
-	<component ref="component" v-if="compiledTemplate" :is="compiledTemplate" v-bind="$attrs"></component>
-	<div ref="component" v-else v-html="props.html" v-bind="$attrs"></div>
+	<component ref="component" v-if="compiledTemplate" :is="compiledTemplate"></component>
+	<div ref="component" v-else v-html="props.html" v-bind="attrs"></div>
 </template>
 <script setup lang="ts">
-import { computed, ref, compile } from "vue"
+import { computed, ref, compile, useAttrs, mergeProps } from "vue"
 import type { HTMLProps } from "@/types/studio_components/HTML"
 
 const component = ref<HTMLElement | null>(null)
 const props = defineProps<HTMLProps>()
+const attrs = useAttrs()
 defineOptions({
 	inheritAttrs: false,
 })
@@ -15,7 +16,15 @@ defineOptions({
 const compiledTemplate = computed(() => {
 	if (!props.html) return null
 	try {
-		return compile(props.html, { hoistStatic: true })
+		const compileFn = compile(props.html, { hoistStatic: true })
+		return (componentProps: any, ctx: any) => {
+			// @ts-ignore
+			const vnode = compileFn!(componentProps, ctx)
+			if (vnode && typeof vnode === "object" && !Array.isArray(vnode)) {
+				vnode.props = mergeProps(vnode.props || {}, attrs)
+			}
+			return vnode
+		}
 	} catch (e) {
 		console.log("Error compiling template:", e)
 		return null
